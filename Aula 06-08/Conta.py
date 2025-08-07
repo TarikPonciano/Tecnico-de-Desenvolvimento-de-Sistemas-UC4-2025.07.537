19ea5172542ef825cdc2e5d052eba2fa1290c4ff
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-08-06 21:24:58 -0300

    Create Conta.py

---
+++ Aula 06-08/Conta.py
@@ -0,0 +1,34 @@
+class Conta:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+
+    def ver_saldo(self):
+        return f"Saldo: R$ {self.saldo:.2f}"
+
+    def depositar(self, valor):
+        if valor <= 0:
+            print("Valor do depósito deve ser positivo!")
+            return
+        else:
+            self.saldo += valor
+            print(f"Depósito de R$ {valor:.2f} realizado com sucesso!")
+
+    def sacar(self, valor):
+        if valor <= 0:
+            print("Valor do saque deve ser positivo!")
+            return
+        elif valor > self.saldo:
+            print("Saldo insuficiente para saque!")
+            return
+        else:
+            self.saldo -= valor
+            print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
+
+
+if __name__ == "__main__":
+    conta = Conta("João", 1000.00)
+    conta.depositar(2000)
+    conta.depositar(-500)
+    print(conta.ver_saldo())
+    conta.sacar(50000)
